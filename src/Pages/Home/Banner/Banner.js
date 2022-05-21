932f1cccb8e318f0153e9296f41e30c017eb1732
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-22 00:03:33 +0600

    banner animated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,12 +5,23 @@ import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
+import { motion } from 'framer-motion';
 // import required modules
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 //import css
 import './Banner.css';
 const Banner = () => {
+    const slideId = [
+        {
+            id: 'slide1'
+        },
+        {
+            id: 'slide2'
+        },
+        {
+            id: 'slide3'
+        },
+    ];
     return (
         <div>
             <Swiper
@@ -19,28 +30,35 @@ const Banner = () => {
                 pagination={{
                     clickable: true,
                 }}
-                autoplay={{
-                    delay: 1500,
-                    disableOnInteraction: false,
-                }}
+
                 modules={[EffectFade, Autoplay, Navigation, Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div id="slide1">
-                        <div className="overlay"></div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div id="slide2">
-                        <div className="overlay"></div>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div id="slide3">
-                        <div className="overlay"></div>
-                    </div>
-                </SwiperSlide>
+                {
+                    slideId.map(slide => <SwiperSlide>
+                        {({ isActive }) => (
+                            <motion.div id={slide.id} initial={{ opacity: 1, scale: 1 }}
+                                animate={{
+                                    opacity: isActive ? 1 : 1,
+                                    scale: isActive ? 1 : 1,
+                                }}>
+                                <div className="overlay flex justify-center items-center">
+                                    <div className="text-part w-[50%]">
+                                        <motion.h1 initial={{ x: '-100vw' }}
+                                            animate={{
+                                                x: isActive ? 0 : '-100vw'
+                                            }}
+                                            className="text-7xl font-lily text-white">
+                                            Blogs
+                                        </motion.h1>
+                                    </div>
+                                </div>
+
+                            </motion.div>
+                        )}
+                    </SwiperSlide>)
+                }
+
 
             </Swiper>
         </div>
