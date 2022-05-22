41cfab72cfc3370cda0d03804513a19f18c9cdcb
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-22 20:50:20 +0600

    added purchase page

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -53,25 +53,40 @@ const Purchase = () => {
                                 </span>
                             </div>
                             <p class="leading-relaxed">{product?.desc}</p>
-                            <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                                    <div class="flex">
+                                        <span class="mr-3">Available:</span>
+                                        <span className='text-primary'>{product?.available}</span>
+                                    </div>
+
+                                    <div class="flex ml-6 items-center">
+                                        <span class="mr-3" >Quantity</span>
+                                        <input type="number" min={product?.minOrder} max={product?.available} placeholder="Minimum Quantity" defaultValue={product?.minOrder} {...register("quantity", {
+                                            required: {
+                                                value: true,
+                                                message: 'Please select the quantity'
+                                            },
+                                            min: {
+                                                value: parseInt(product?.minOrder),
+                                                message: `You have have order minimum ${product?.minOrder} ` // JS only: <p>error message</p> TS only support string
+                                            },
+                                            max: {
+                                                value: parseInt(product?.available),
+                                                message: `You can't order than ${product?.available} `
+                                            }
+                                        })} class={errors?.quantity ? 'input input-bordered input-error w-full max-w-xs' : 'input w-full max-w-xs'} />
+
+                                    </div>
+
+                                </div>
+
                                 <div class="flex">
-                                    <span class="mr-3">Available:</span>
-                                    <span className='text-primary'>{product?.available}</span>
+                                    <span class="title-font font-medium text-2xl">${product?.price}/unit</span>
+                                    <button type='submit' class="flex ml-auto bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary disabled:bg-neutral rounded" disabled={errors?.quantity}>Purchase</button>
                                 </div>
-                                <div class="flex ml-6 items-center">
-                                    <span class="mr-3">Quantity</span>
-                                    <input type="text" placeholder="Minimum Quantity" defaultValue={product?.minOrder} {...register("quantity")} class="input w-full max-w-xs" />
-                                </div>
-                            </div>
-                            <div class="flex">
-                                <span class="title-font font-medium text-2xl">${product?.price}/unit</span>
-                                <button class="flex ml-auto       bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-accent rounded">Purchase</button>
-                                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center ml-4">
-                                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                                    </svg>
-                                </button>
-                            </div>
+                                <p className='text-error'>{errors?.quantity?.message}</p>
+                            </form>
                         </div>
                     </div>
                 </div>
