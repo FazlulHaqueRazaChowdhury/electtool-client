cfdb2fbbe6106e96e3ffbfb03e722f5263614e05
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-06-24 14:38:08 +0600

    fixing bugs

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,8 +3,8 @@ import './Loading.css';
 const Loading = () => {
     return (
         <div className="flex justify-center items-center">
-            <div class="loadingio-spinner-double-ring-k39yzfouwos">
-                <div class="ldio-yj272sfd67">
+            <div className="loadingio-spinner-double-ring-k39yzfouwos">
+                <div className="ldio-yj272sfd67">
                     <div></div>
                     <div></div>
                     <div><div></div></div>
